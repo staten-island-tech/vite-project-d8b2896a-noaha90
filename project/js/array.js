c5f05463d653c1e7c1247d75117c86534fe6092c
noaha90 <noahabb@outlook.com>
2023-12-05 21:48:40 -0500

    w
    w

--- project/js/array.js
+++ project/js/array.js
@@ -2,7 +2,7 @@ const array = [
     {
         bill: "One Dollar",
         price: "1.01",
-        image: "1",
+        image: "https://upload.wikimedia.org/wikipedia/commons/2/23/US_one_dollar_bill%2C_obverse%2C_series_2009.jpg",
         tagline: "One Dollar Bill!",
         border: "im",
         currency: "$",
@@ -11,7 +11,7 @@ const array = [
     {
         bill: "Two Dollars",
         price: "2.01",
-        image: "2",
+        image: "https://upload.wikimedia.org/wikipedia/commons/2/27/US_%242_bill_obverse_series_2003_A.jpg",
         tagline: "Two Dollar Bill!",
         border: "im",
         currency: "$",
@@ -20,7 +20,7 @@ const array = [
     {
         bill: "Five Dollars",
         price: "5.01",
-        image: "5",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/US_%245_series_2003A_obverse.jpg/1200px-US_%245_series_2003A_obverse.jpg",
         tagline: "Five Dollar Bill!",
         border: "im",
         currency: "$",
@@ -29,7 +29,7 @@ const array = [
     {
         bill: "Five Pounds",
         price: "6.30",
-        image: "pound",
+        image: "https://www.foreigncurrencyandcoin.com/wp-content/uploads/2018/12/products-15892.jpg",
         tagline: "Five Pounds!",
         border: "im",
         currency: "£",
@@ -38,7 +38,7 @@ const array = [
     {
         bill: "Ten Dollars",
         price: "10.01",
-        image: "10",
+        image: "https://en.numista.com/catalogue/photos/etats-unis/5e951db46ddf53.96848322-original.jpg",
         tagline: "Ten Dollar Bill!",
         border: "im",
         currency: "$",
@@ -47,7 +47,7 @@ const array = [
     {
         bill: "Twenty Dollars",
         price: "20.01",
-        image: "20",
+        image: "https://upload.wikimedia.org/wikipedia/commons/7/79/US_%2420_Series_2006_Obverse.jpg",
         tagline: "Twenty Dollar Bill!",
         border: "im",
         currency: "$",
@@ -56,16 +56,16 @@ const array = [
     {
         bill: "Bass",
         price: "30.01",
-        image: "bass",
+        image: "https://i.imgur.com/NnSuIAv.png",
         tagline: "30 Dollar Bass Pro Shop Gift Card!",
         border: "im",
         currency: "$",
-        type: ["sort by if i could beat the person in a fight","show all"],
+        type: ["show all"],
     },
     {
         bill: "Fifty Dollars",
         price: "50.01",
-        image: "50",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/50_USD_Series_2004_Note_Front.jpg/800px-50_USD_Series_2004_Note_Front.jpg",
         tagline: "Take a Wild Guess",
         border: "im",
         currency: "$",
@@ -74,7 +74,7 @@ const array = [
     {
         bill: "One Hundred Dollars",
         price: "100.01",
-        image: "100",
+        image: "https://upload.wikimedia.org/wikipedia/commons/4/4d/Usdollar100front.jpg",
         tagline: "One Hundred Dollar Bill!",
         border: "im",
         currency: "$",
@@ -83,7 +83,7 @@ const array = [
     {
         bill: "Whalen Dollar",
         price: "101.01",
-        image: "whalen",
+        image: "https://i.imgur.com/q7yp6a3.jpg",
         tagline: "art in its purest form",
         border: "im",
         currency: "$",
@@ -92,19 +92,19 @@ const array = [
     {
         bill: "Thousand Dollar",
         price: "1000.01",
-        image: "1000",
+        image: "https://www.investopedia.com/thmb/0zt7q_YLmyUyqk0XtlbWuE_b3YY=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/1000-59f619aa845b34001122fbb7.jpg",
         tagline: "Vintage Thousand Dollar Bill!",
-        border: "borderlessim",
+        border: "im",
         currency: "$",
         type: ["presidents only","sort by if i could beat the person in a fight","find bills over 50 units","show all"],
     },
     {
         bill: "Poke Dollar",
         price: "1,000,000.1",
-        image: "bike",
+        image: "https://i.imgur.com/9WYMAiO.png",
         tagline: "Cerulean City Bike Voucher!",
         border: "borderlessim",
-        currency: '<img src="d/PokeDollar.jpg">',
+        currency: '<img src="https://i.imgur.com/SYgj0IE.png">',
         type: ["find bills over 50 units","show all"],
     },
     
